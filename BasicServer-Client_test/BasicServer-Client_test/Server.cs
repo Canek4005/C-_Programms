9f0ea969ab38a869ef04f3d1fdc68c9df0d9579b
Alexandr Zaycev <aleksandr-zaycev-1999@mail.ru>
2020-11-16 13:32:49 +0300

    BasicServerC#

---
+++ BasicServer-Client_test/BasicServer-Client_test/Server.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace BasicServer_Client_test
+{
+    class Server
+    {
+        static int port = 4545; // порт для приема входящих запросов
+        
+        static void Main(string[] args)
+        {
+            //получаем адрес запуска сервера
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+            //создаем сокет
+            Socket listenSoket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                //связываем сокет с локальной точкой, по которой будем принимать данные
+                listenSoket.Bind(ipPoint);
+                //начинаем прослушивание (запускаем сервер)
+                listenSoket.Listen(10);
+
+                Console.WriteLine("Server is listening...");
+
+                while (true)
+                {
+                    Socket handler = listenSoket.Accept();
+                    //получение сообщения
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;//количество полученных байтов
+                    byte[] data = new byte[256];//Буфер для получаемых данных
+
+                    do
+                    {
+                        bytes = handler.Receive(data);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (handler.Available > 0);
+                    Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
+                    //отправляем ответ
+                    string message = "Сообщение доставлено";
+                    data = Encoding.Unicode.GetBytes(message);
+                    handler.Send(data);
+                    //закрываем сокет
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ BasicServer-Client_test/Client/Client.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace BasicServer_Client_test
+{
+    class Client
+    {
+        static int port = 4545; // порт для приема входящих запросов
+        static string adress = "127.0.0.1";//адрес сервера
+        static void Main(string[] args)
+        {
+            try
+            {
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(adress), port);
+
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                socket.Connect(ipPoint);
+                Console.WriteLine("Введите Сообщение");
+                string message = Console.ReadLine();
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                socket.Send(data);
+
+                data = new byte[256];
+                StringBuilder builder = new StringBuilder();
+                int bytes = 0;
+
+                do
+                {
+                    bytes = socket.Receive(data, data.Length, 0);
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                }
+                while (socket.Available > 0);
+                Console.WriteLine("Ответ от сервера: " + builder.ToString());
+
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+
+        }
+
+    }
+}
