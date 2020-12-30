de3212c6a31481bb9e07d9163716434670f4c57d
Alexandr Zaycev <aleksandr-zaycev-1999@mail.ru>
2020-12-30 19:59:18 +0300

    CW1

--- ConsoleCW/ConsoleCW/Program.cs
+++ ConsoleCW/ConsoleCW/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace ConsoleCW
 {
@@ -6,7 +8,31 @@ namespace ConsoleCW
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(DuplicateCount("Indivisabilit"));
+        }
+
+        public static int DuplicateCount(string str)
+        {
+            
+            var test = new HashSet<char>();
+            str = str.ToLower();
+            for(int i = 0; i < str.Length; i++)
+            {
+                for (int j = i+1; j < str.Length; j++)
+                {
+                    
+                    if (str[i] == str[j]&&test.Contains(str[i])==false)
+                    {
+                        
+                        test.Add(str[i]);
+                        break;
+                    }
+                }
+            }
+            
+
+            
+            return test.Count;
         }
     }
 }
