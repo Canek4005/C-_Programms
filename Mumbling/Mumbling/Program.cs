1d7f16c46143fdc5f5196c062ce9a7398ac8cc8b
Alexandr Zaycev <aleksandr-zaycev-1999@mail.ru>
2021-01-01 13:53:14 +0300

    Task_Mummbling

---
+++ Mumbling/Mumbling/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Mumbling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Accum("aBcD"));
+        }
+        public static String Accum(string s)
+        {
+            string str=null;
+            
+            int j = 0;
+            foreach (char c in s.ToLower())
+            {
+                if(j>0)
+                    str += "-";
+                str += Convert.ToString(c).ToUpper();
+                for (int i = 0; i < j; i++)
+                {
+
+                    str += c;
+                }
+                                
+                j += 1;
+            }
+            // your code
+            return str;
+        }
+    }
+}
---
+++ UniqueInOrder/UniqueInOrder/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace UniqueInOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(UniqueInOrder("AAAABBBCCDAABBB"));
+        }
+        public static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+        {
+            var keeper = new HashSet<char>();
+            string text = iterable.ToString();
+            IEnumerable str;
+            IEnumerable items;
+            foreach (char c in text)
+            {
+                if (keeper.Contains(c)!)
+                {
+                    keeper.Add(c);
+                    str.Add(c);
+                    str = str.Concat(new[] { "foo" });
+                    items = items.Add("bar");
+                    
+                }
+            }
+
+
+
+
+        }
+    }    
+}
