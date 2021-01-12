f206a5687f8d44740937a862f517c48f5ad9692e
Alexandr Zaycev <aleksandr-zaycev-1999@mail.ru>
2021-01-12 20:15:57 +0300

    StartOOPTanks

---
+++ CodeWars/Vowel Count/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Vowel_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetVowelCount("abracadabra"));
+        }
+        public static int GetVowelCount(string str)
+        {
+           
+            char[] alfabet = new char[] { 'a', 'e', 'i', 'o', 'u' };
+            
+            return (from t in str where alfabet.Contains(t) select t).Count();
+        }
+    }
+}
