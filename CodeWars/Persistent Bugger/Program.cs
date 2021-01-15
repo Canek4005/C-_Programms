8e0e978be337f9777ed05ca15a8ea127399f0e3c
Alexandr Zaycev <aleksandr-zaycev-1999@mail.ru>
2021-01-15 20:25:20 +0300

    task

---
+++ CodeWars/Persistent Bugger/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Persistent_Bugger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Persistence(39));
+        }
+        public static int Persistence(long n)
+        {
+            
+            int i = 0;
+            if(n.ToString().Length>1)
+            do
+            {
+                    var sum = 1;
+                foreach (char s in n.ToString())
+                {
+                    sum *= int.Parse(s.ToString());
+                    
+                }
+                i++;
+                n = Convert.ToInt64(sum);
+            }
+            while (n>=10);
+
+            return i;
+        }
+    }
+}
---
+++ CodeWars/SumOfDigits/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace SumOfDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DigitalRoot(1235685));
+        
+        
+        }
+        public static int DigitalRoot(long n)
+        {
+            int sum = 0;
+            var mes = n.ToString();
+            foreach (char i in mes)
+            {
+                sum += int.Parse(i.ToString());
+            }
+            
+            if ((sum / 10) >= 1)
+            {
+               return DigitalRoot(sum);
+                
+            }
+            else
+            {
+                return sum;
+                
+            }
+            
+            
+
+            
+        }
+    }
+}
